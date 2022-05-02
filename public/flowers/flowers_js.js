d6f5fd995d4b263d26e33d765fe54bde005c70dc
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-05-02 15:13:43 +0530

    Added carousel to all the pages except home page

--- public/flowers/flowers_js.js
+++ public/flowers/flowers_js.js
@@ -76,3 +76,117 @@ jQuery(document).ready(function ($) {
 //         }
 //     })
 // });
+
+$(function(){
+	polaroidSlides();
+  });
+  //var currentSlide;
+  function polaroidSlides() {
+	$polaroidSlide = $(".polaroid");
+	currentSlide = 0;
+	polaroidSlidesActive = true;
+	TweenMax.set($polaroidSlide[1], {left:"60%", opacity:0.5, scale:0.5});
+	TweenMax.set($polaroidSlide[2], {left:"-60%", opacity:0.5, scale:0.5});
+	TweenMax.set($polaroidSlide[3], {opacity:0, scale:0});
+	//TweenMax.set($polaroidSlide[4], {opacity:0, scale:0});
+  };
+  
+  function nextPolaroid() {
+	  TweenMax.to($polaroidSlide.eq(currentSlide), 1, {left:"-60%", opacity:0.5, scale:0.5});
+	  var prevSlide = currentSlide;
+	  if (currentSlide < $polaroidSlide.length - 1) {
+		currentSlide++;
+	  }
+	  else {
+		currentSlide = 0;      
+	  };
+	  if (currentSlide < $polaroidSlide.length - 1) {
+		var followingSlide = currentSlide+1;
+	  }
+	  else {
+		followingSlide = 0;
+	  }
+	
+	
+	 if ($polaroidSlide.length >= 4) {
+	  if (currentSlide == 0) {
+		var left = $polaroidSlide.length - 1;
+	  }
+	  else {
+		left = currentSlide - 1;
+	  }
+	  if (currentSlide == $polaroidSlide.length - 1) {
+		var right = 0;
+	  }
+	  else {
+		right = currentSlide + 1;
+	  }
+	  var range = [];
+	  for (i = 0; i < $polaroidSlide.length; i++) {
+		range.push(i);
+	  }
+	  
+	  for (i = 0; i < range.length; i++) {
+		if ((range[i] != left) && (range[i] != right) && (range[i] != currentSlide)) {
+		  TweenMax.set($polaroidSlide[i], {opacity:0, scale:0});
+		}
+  
+	  }
+	}
+	  TweenMax.fromTo($polaroidSlide.eq(followingSlide),1, {left:"100%", opacity:0,scale:0}, {left:"60%", opacity:0.5,scale:0.5});
+	  TweenMax.fromTo($polaroidSlide.eq(currentSlide), 1, {left: "60%", opacity:0.5, scale:0.5}, {left:"0px", opacity:1, scale:1});
+	}
+	
+	
+	
+	
+	
+	
+  function previousPolaroid() {
+	  TweenMax.fromTo($polaroidSlide.eq(currentSlide), 1, {left:0, opacity:1, scale:1},{left:"60%", opacity:0.5, scale:0.5});
+	  var prevSlide = currentSlide;
+	  if (currentSlide > 0) {
+		currentSlide--;
+	  }
+	  else {
+		currentSlide = $polaroidSlide.length - 1;
+	  };
+	  if (currentSlide > 0) {
+		var followingSlide = currentSlide - 1;
+	  }
+	  else {
+		followingSlide = $polaroidSlide.length - 1;
+	  }
+	
+	
+	if ($polaroidSlide.length >= 4) {
+	  if (currentSlide == 0) {
+		var left = $polaroidSlide.length - 1;
+	  }
+	  else {
+		left = currentSlide - 1;
+	  }
+	  if (currentSlide == $polaroidSlide.length - 1) {
+		var right = 0;
+	  }
+	  else {
+		right = currentSlide + 1;
+	  }
+	  var range = [];
+	  for (i = 0; i < $polaroidSlide.length; i++) {
+		range.push(i);
+	  }
+  
+	  for (i = 0; i < range.length; i++) {
+		if ((range[i] != left) && (range[i] != right) && (range[i] != currentSlide)) {
+		  TweenMax.set($polaroidSlide[i], {opacity:0, scale:0});
+		}
+  
+	  }
+	}
+	
+	console.log("Slide to the left: " + followingSlide);
+	
+	TweenMax.fromTo($polaroidSlide.eq(followingSlide),1, {left:"-100%", opacity:0,scale:0}, {left:"-60%", opacity:0.5,scale:0.5});
+	  TweenMax.fromTo($polaroidSlide.eq(currentSlide), 1, {left: "-60%", opacity:0.5, scale:0.5}, {left:"0px", opacity:1, scale:1});
+	}
