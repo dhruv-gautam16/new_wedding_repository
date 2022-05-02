ab7d1bd57e451bafaea2f717268cb94bcdeef443
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-05-02 14:17:55 +0530

    Useless commit

--- public/home_js.js
+++ public/home_js.js
@@ -63,27 +63,5 @@ jQuery(document).ready(function ($) {
 
 
 // Js code for scroll effect
-$(window).on('scroll', function()
-{	
-	add_onscroll_effect($('.scrolleffect1'), 'open', 767, 200);
-  add_onscroll_effect($('.scrolleffect2'), 'open', 767, 200);
-});
-function add_onscroll_effect(obj, trigger_class, win_width=0, scarto=0)
-{
-	if ( $(window).width() < win_width ) return;
-	if ( obj.length == 0 ) return;
 
-	var currOffset 	= 0,
-		win_scroll 	= $(window).scrollTop();
-
-	obj.each(function(index)
-	{
-		currOffset = ( $(this).offset().top - scarto );
-
-		if ( win_scroll > currOffset )
-		{
-			$(this).addClass(trigger_class);
-		}
-	});
-}
 // Js scroll code end
