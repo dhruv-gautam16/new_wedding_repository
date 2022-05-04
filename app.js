b18de3f1e869d9184bc206fa06444ada308f03ff
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-05-05 02:00:49 +0530

    Added razorpay to the website

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Razorpay = require('razorpay');
 const app = express();
 const path = require('path');
 
@@ -30,6 +31,42 @@ app.get('/registrationform',(req,res)=>{
 
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, 'views'));
+
+// Razropay Integration
+var instance = new Razorpay({
+    key_id: 'YOUR_KEY_ID',
+    key_secret: 'YOUR_KEY_SECRET',
+  });
+
+app.post('/create/orderId',(req,res)=>{
+    console.log("Create orderId request",req.body);
+    var options = {
+        amount: req.body.amount,  // amount in the smallest currency unit
+        currency: "INR",
+        receipt: "rcp1"
+      };
+      instance.orders.create(options, function(err, order) {
+        console.log(order);
+        res.send({orderId:order.id});
+      });
+})
+app.post("/api/payment/verify",(req,res)=>{
+
+    let body=req.body.response.razorpay_order_id + "|" + req.body.response.razorpay_payment_id;
+   
+     var crypto = require("crypto");
+     var expectedSignature = crypto.createHmac('sha256', '<YOUR_API_SECRET>')
+                                     .update(body.toString())
+                                     .digest('hex');
+                                     console.log("sig received " ,req.body.response.razorpay_signature);
+                                     console.log("sig generated " ,expectedSignature);
+     var response = {"signatureIsValid":"false"}
+     if(expectedSignature === req.body.response.razorpay_signature)
+      response={"signatureIsValid":"true"}
+         res.send(response);
+     });
+   
+   
 
 app.listen(3000,()=>{
     console.log("Listening to the port 3000 ");
